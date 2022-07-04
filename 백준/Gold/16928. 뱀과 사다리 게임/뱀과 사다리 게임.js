effc455446094698f1aa9ed4356b43820849cb42
MinKyu Tae <alsrb8442@naver.com>
2022-07-04 17:37:16 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 136 ms, Memory: 9404 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.js
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.js
@@ -7,29 +7,6 @@ let input = require("fs")
 
 const [N, M] = input[0].split(" ").map(Number);
 
-const up = new Map();
-const down = new Map();
-
-// key, value로 수정.
-for (let i = 0; i < N; i++) {
-  const [start, dest] = input[i + 1].split(" ").map(Number);
-  up.set(start, dest);
-}
-
-for (let i = 0; i < M; i++) {
-  const [start, dest] = input[i + N + 1].split(" ").map(Number);
-  down.set(start, dest);
-}
-
-// 기본 이동 수는 1 ~ 6까지 6가지
-// 이동하다가 사다리 만나면 이동 뱀은 건너뛰여야 함.
-// BFS 이용
-const board = new Array(101).fill(999);
-
-bfs();
-
-console.log(board[100]);
-
 function bfs() {
   // 큐 원소 : [위치, cnt]
   const q = [[1, 0]];
@@ -43,22 +20,43 @@ function bfs() {
       if (board[dest] <= cnt + 1) continue;
       if (dest > 100) continue;
 
+      // 사다리 이동
       if (up.has(dest)) {
         const upDest = up.get(dest);
-
-        // if (board[upDest] < cnt + 1) continue;
-
         q.push([upDest, cnt + 1]);
         board[upDest] = cnt + 1;
-      } else if (down.has(dest)) {
+      }
+      // 뱀 이동
+      else if (down.has(dest)) {
         const downDest = down.get(dest);
-
         q.push([downDest, cnt + 1]);
         board[dest] = cnt + 1;
-      } else {
+      }
+      // 그냥 주사위 이동.
+      else {
         q.push([dest, cnt + 1]);
         board[dest] = cnt + 1;
       }
     }
   }
 }
+
+// BFS 의 방문 체크용 배열
+const board = new Array(101).fill(999);
+const up = new Map();
+const down = new Map();
+
+// 사다리와 뱀을 map으로 저장.
+for (let i = 0; i < N; i++) {
+  const [start, dest] = input[i + 1].split(" ").map(Number);
+  up.set(start, dest);
+}
+
+for (let i = 0; i < M; i++) {
+  const [start, dest] = input[i + N + 1].split(" ").map(Number);
+  down.set(start, dest);
+}
+
+bfs();
+
+console.log(board[100]);
