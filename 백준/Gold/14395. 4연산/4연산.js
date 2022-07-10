30a11f009c1a24ef9e6085d63187da92f750d3b2
MinKyu Tae <alsrb8442@naver.com>
2022-07-11 00:05:29 +0900

    [Gold V] Title: 4연산, Time: 136 ms, Memory: 9380 KB -BaekjoonHub

--- 백준/Gold/14395. 4연산/4연산.js
+++ 백준/Gold/14395. 4연산/4연산.js
@@ -13,6 +13,9 @@ if (cur === dest) {
 }
 
 const MAX = Math.max(cur, dest);
+// 방문처리용
+// map과 set 중 map이 해시 검색으로 더 빠를 것 같긴한데..
+// Set으로 해도 통과.
 const checkSet = new Set();
 
 function bfs() {
@@ -27,7 +30,6 @@ function bfs() {
     const mul = s * s;
     if (mul <= MAX && !checkSet.has(mul)) {
       q.push([mul, result + "*"]);
-      // visited[mul] = true;
       checkSet.add(mul);
     }
 
@@ -37,10 +39,9 @@ function bfs() {
       checkSet.add(plus);
     }
 
-    // ? 항상 0인데 의미가 잇나?
-    // const minus = s - s;
+    // 빼기는 결과가 항상 0이라 의미가 없음.
 
-    // 나누기
+    // 나누기 : 한 번 실행.
     if (!checkSet.has(1)) {
       q.push([1, result + "/"]);
       checkSet.add(1);
