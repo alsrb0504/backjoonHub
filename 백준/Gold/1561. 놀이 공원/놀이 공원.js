fc4305579196e69740736aac9b6c307195887cb2
MinKyu Tae <alsrb8442@naver.com>
2022-09-05 19:47:13 +0900

    [Gold II] Title: 놀이 공원, Time: 200 ms, Memory: 12260 KB -BaekjoonHub

---
+++ 백준/Gold/1561. 놀이 공원/놀이 공원.js
@@ -0,0 +1,58 @@
+const readFileSyncAddress = '/dev/stdin';
+
+let input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const nums = input[1].split(" ").map(Number);
+
+// 예외 : 아이들의 수가 놀이기구 수보다 작을 경우.
+if (N < M) {
+  console.log(N);
+  return;
+}
+
+const max = Math.max.apply(null, nums);
+
+let answer = 0;
+let start = 0;
+// let end = max * Math.floor(N / M);
+let end = max * N;
+
+let minTime = Infinity;
+
+while (start <= end) {
+  const mid = Math.floor((start + end) / 2);
+
+  let cnt = M;
+
+  nums.forEach((n) => {
+    cnt += Math.floor(mid / n);
+  });
+
+  if (cnt >= N) {
+    minTime = Math.min(minTime, mid);
+    end = mid - 1;
+  } else {
+    start = mid + 1;
+  }
+}
+
+let prevCnt = M;
+nums.forEach((n) => {
+  prevCnt += Math.floor((minTime - 1) / n);
+});
+
+nums.forEach((n, idx) => {
+  if (minTime % n === 0) {
+    prevCnt++;
+  }
+
+  if (prevCnt === N && answer === 0) {
+    answer = idx + 1;
+  }
+});
+
+console.log(answer);
