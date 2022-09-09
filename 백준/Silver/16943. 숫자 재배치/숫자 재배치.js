f49628e867e4a5c9dc2b04359f01a95603265fdd
MinKyu Tae <alsrb8442@naver.com>
2022-09-09 15:23:32 +0900

    [Silver I] Title: 숫자 재배치, Time: 328 ms, Memory: 12520 KB -BaekjoonHub

--- 백준/Silver/16943. 숫자 재배치/숫자 재배치.js
+++ 백준/Silver/16943. 숫자 재배치/숫자 재배치.js
@@ -1,29 +1,25 @@
 const readFileSyncAddress = '/dev/stdin';
-
 let input = require("fs")
   .readFileSync(readFileSyncAddress)
   .toString()
   .split("\n");
 
 const [A, B] = input[0].split(" ").map(Number);
-
 const nums = A.toString().split("").map(Number);
 const visited = new Array(nums.length).fill(false);
-const stack = [];
 let answer = -1;
 
-dfs(0);
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] !== 0) {
+    visited[i] = true;
+    dfs(1, nums[i]);
+    visited[i] = false;
+  }
+}
 
-function dfs(cnt) {
+function dfs(cnt, acc) {
   if (cnt === nums.length) {
-    if (stack[0] === 0) return;
-
-    const num = Number(stack.join(""));
-
-    if (num < B) {
-      answer = Math.max(answer, num);
-    }
-
+    if (acc < B && answer < acc) answer = acc;
     return;
   }
 
@@ -31,12 +27,8 @@ function dfs(cnt) {
     if (visited[i]) continue;
 
     visited[i] = true;
-    stack.push(nums[i]);
-
-    dfs(cnt + 1);
-
+    dfs(cnt + 1, acc * 10 + nums[i]);
     visited[i] = false;
-    stack.pop();
   }
 }
 
