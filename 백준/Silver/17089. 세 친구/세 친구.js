b37b9d31fc90e5ac5561e306f2d801e9d8f198cb
MinKyu Tae <alsrb8442@naver.com>
2022-09-15 16:29:52 +0900

    [Silver I] Title: 세 친구, Time: 216 ms, Memory: 12796 KB -BaekjoonHub

--- 백준/Silver/17089. 세 친구/세 친구.js
+++ 백준/Silver/17089. 세 친구/세 친구.js
@@ -10,11 +10,8 @@ const set = new Set();
 let answer = Infinity;
 
 function insertMap(a, b) {
-  if (map.has(a)) {
-    map.get(a).push(b);
-  } else {
-    map.set(a, [b]);
-  }
+  if (map.has(a)) map.get(a).push(b);
+  else map.set(a, [b]);
 }
 
 for (let i = 1; i <= M; i++) {
@@ -28,33 +25,31 @@ for (let i = 1; i <= M; i++) {
 }
 
 const nums = [...set];
-const stack = [];
 
 for (let i = 0; i < nums.length; i++) {
-  dfs(1, nums[i], map.get(nums[i]).length, nums[i]);
-}
+  const fir = nums[i];
+  const firArr = map.get(fir);
 
-function dfs(cnt, cur, acc, start) {
-  if (cnt === 3) {
-    const lastArr = map.get(cur);
+  if (firArr.length < 2) continue;
 
-    if (lastArr.findIndex((v) => v === start) !== -1) {
-      answer = Math.min(answer, acc - 6);
+  for (let j = 0; j < firArr.length; j++) {
+    const sec = firArr[j];
+    const secArr = map.get(sec);
+
+    if (secArr.length < 2) continue;
+
+    for (let k = 0; k < secArr.length; k++) {
+      const thi = secArr[k];
+      const thiArr = map.get(thi);
+
+      for (let t = 0; t < thiArr.length; t++) {
+        if (fir === thiArr[t]) {
+          const sum = firArr.length + secArr.length + thiArr.length;
+          answer = Math.min(answer, sum - 6);
+        }
+      }
     }
-
-    return;
   }
-
-  const arr = map.get(cur);
-
-  if (arr.length < 2) return;
-
-  arr.forEach((v) => {
-    if (v !== start) {
-      const length = map.get(v).length;
-      dfs(cnt + 1, v, acc + length, start);
-    }
-  });
 }
 
 console.log(answer === Infinity ? -1 : answer);
