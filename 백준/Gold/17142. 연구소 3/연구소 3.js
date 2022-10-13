45f5a32693acffcc7d37086aba42895a9cecf6fd
MinKyu Tae <alsrb8442@naver.com>
2022-10-13 16:00:20 +0900

    [Gold IV] Title: 연구소 3, Time: 388 ms, Memory: 15992 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.js
+++ 백준/Gold/17142. 연구소 3/연구소 3.js
@@ -11,13 +11,9 @@ const virusPos = [];
 
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
-    // if (map[i][j] === 1) visited[i][j] = true;
     if (map[i][j] === 2) virusPos.push([i, j]);
   }
 }
-
-// 조합으로 바이러스 위치를 지정 후, 호출?
-// 50 * 50 => 2500 C 10 = 시간초과
 
 let answer = Number.MAX_SAFE_INTEGER;
 
@@ -28,12 +24,10 @@ const virusStack = [];
 
 function dfs(cnt, start) {
   if (cnt === M) {
-    // console.log(bfs());
     const result = bfs();
 
     if (result !== -1) answer = Math.min(answer, result);
     return;
-    // bfs();
   }
 
   for (let i = start; i < virusPos.length; i++) {
@@ -45,29 +39,17 @@ function dfs(cnt, start) {
 
 function bfs() {
   const visited = Array.from({ length: N }, () => new Array(N).fill(false));
-
-  const numbers = Array.from({ length: N }, () => new Array(N).fill(-1));
-
   const q = [];
-
   let maxCnt = 0;
-
-  const start = [];
 
   virusStack.forEach((el) => {
     const [sy, sx] = virusPos[el];
-
-    start.push([sy, sx]);
-    numbers[sy][sx] = 0;
-
     q.push([sy, sx, 0]);
     visited[sy][sx] = true;
   });
 
   while (q.length) {
     const [y, x, cnt] = q.shift();
-
-    // maxCnt = Math.max(maxCnt, cnt);
 
     for (let i = 0; i < 4; i++) {
       const [ny, nx] = [y + dy[i], x + dx[i]];
@@ -76,36 +58,13 @@ function bfs() {
       if (map[ny][nx] === 1 || visited[ny][nx]) continue;
 
       if (map[ny][nx] === 2) {
-        // q.unshift([ny, nx, cnt]);
-        // maxCnt = Math.max(maxCnt, cnt + 1);
-        // numbers[ny][nx] = cnt;
-
-        // q.unshift([ny, nx, cnt + 1]);
-
         // 이미 모든 칸이 바이러스로 차 있다면 종료
-        if (checkEnd()) {
-          // console.log("end : ", maxCnt);
-
-          // if (maxCnt === 3) {
-          //   console.log(start);
-
-          //   console.table(map);
-          //   console.table(numbers);
-          // }
-
-          return maxCnt;
-        }
-
-        q.push([ny, nx, cnt + 1]);
-      } else {
-        q.push([ny, nx, cnt + 1]);
+        if (checkEnd()) return maxCnt;
       }
 
+      q.push([ny, nx, cnt + 1]);
       maxCnt = Math.max(maxCnt, cnt + 1);
-
-      numbers[ny][nx] = cnt + 1;
       visited[ny][nx] = true;
-      // q.push([ny, nx, cnt + 1]);
     }
   }
 
@@ -114,11 +73,6 @@ function bfs() {
       if (map[i][j] !== 1 && visited[i][j] === false) return -1;
     }
   }
-
-  // console.log(maxCnt);
-  // console.table(visited);
-  // console.table(map);
-  // console.table(numbers);
 
   return maxCnt;
 
@@ -133,7 +87,5 @@ function bfs() {
   }
 }
 
-// bfs();
 dfs(0, 0);
-
 console.log(answer === Number.MAX_SAFE_INTEGER ? -1 : answer);
