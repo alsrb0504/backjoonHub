32577593e6320bb1ef34c4b5852e1aa5ad6932f6
MinKyu Tae <alsrb8442@naver.com>
2022-11-28 01:07:13 +0900

    [Gold IV] Title: 곰곰아 선 넘지마, Time: 244 ms, Memory: 24516 KB -BaekjoonHub

--- 백준/Gold/26075. 곰곰아 선 넘지마/곰곰아 선 넘지마.js
+++ 백준/Gold/26075. 곰곰아 선 넘지마/곰곰아 선 넘지마.js
@@ -4,87 +4,11 @@ let input = require("fs")
   .toString()
   .split("\n");
 
-const [N, M] = input[0].split(" ").map(Number);
+const [N, _] = input[0].split(" ").map(Number);
 const n1 = input[1].trimEnd().split("");
 const n2 = input[2].trimEnd().split("");
 
-// // 예외처리
-// if (N === 0 || M === 0) {
-//   console.log(0);
-//   return;
-// }
-
-const result_one = oneCalc();
-const result_zero = zeroCalc();
-
-// console.log(`result_one = ${result_one}, result_zero = ${result_zero}`);
-
-console.log(
-  result_one > result_zero ? result_zero.toString() : result_one.toString()
-);
-
-function oneCalc() {
-  // 1인 경우
-  const n1_pos = [];
-  const n2_pos = [];
-  let n1_cnt = 0n;
-  let n2_cnt = 0n;
-
-  n1.forEach((c, idx) => {
-    if (c === "1") n1_pos.push(idx);
-  });
-  n2.forEach((c, idx) => {
-    if (c === "1") n2_pos.push(idx);
-  });
-
-  for (let i = 0; i < M; i++) {
-    const n1_idx = n1_pos[i];
-    const n2_idx = n2_pos[i];
-
-    // const mid = Math.floor((n1_idx + n2_idx) / 2);
-    const n1n2 = n1_idx + n2_idx;
-    const mid = Math.floor(n1n2 / 2);
-
-    // console.log(`n1_idx = ${n1_idx}, n2_idx = ${n2_idx}`);
-    // console.log(`mid = ${mid}`);
-    // console.log(`Math.abs(n1_idx - mid) = ${Math.abs(n1_idx - mid)}`);
-    // console.log(`Math.abs(n2_idx - mid) = ${Math.abs(n2_idx - mid)}`);
-
-    // console.log();
-
-    // MID가 짝수일 경우는 상관이 없는데
-    // 홀수일 경우가 다르네
-
-    const n1_dif = BigInt(Math.abs(n1_idx - mid));
-    const n2_dif = BigInt(Math.abs(n2_idx - mid));
-
-    // if (mid % 2 === 0) {
-    if (n1n2 % 2 === 0) {
-      n1_cnt += n1_dif;
-      n2_cnt += n2_dif;
-    } else {
-      const gt = n1_dif > n2_dif ? n1_dif : n2_dif;
-      const ls = n1_dif > n2_dif ? n2_dif : n1_dif;
-
-      if (n1_cnt < n2_cnt) {
-        n1_cnt += gt;
-        n2_cnt += ls;
-      } else {
-        n1_cnt += ls;
-        n2_cnt += gt;
-      }
-    }
-  }
-
-  const sum = n1_cnt * n1_cnt + n2_cnt * n2_cnt;
-
-  // console.log("one");
-  // console.log(sum.toString());
-  return sum;
-}
-
-function zeroCalc() {
-  // 1인 경우
+function solution() {
   const n1_pos = [];
   const n2_pos = [];
   let n1_cnt = 0n;
@@ -101,19 +25,14 @@ function zeroCalc() {
     const n1_idx = n1_pos[i];
     const n2_idx = n2_pos[i];
 
+    // n1 + n2 의 합이 홀수/짝수인지에 따라 +1 값을 작은 부분에 처리해줘야 함.
     const n1n2 = n1_idx + n2_idx;
     const mid = Math.floor(n1n2 / 2);
-
-    // console.log(`n1_idx = ${n1_idx}, n2_idx = ${n2_idx}`);
-    // console.log(`mid = ${mid}`);
-    // console.log(`Math.abs(n1_idx - mid) = ${Math.abs(n1_idx - mid)}`);
-    // console.log(`Math.abs(n2_idx - mid) = ${Math.abs(n2_idx - mid)}`);
-
-    // console.log();
 
     const n1_dif = BigInt(Math.abs(n1_idx - mid));
     const n2_dif = BigInt(Math.abs(n2_idx - mid));
 
+    // 홀/짝 구분
     if (n1n2 % 2 === 0) {
       n1_cnt += n1_dif;
       n2_cnt += n2_dif;
@@ -132,8 +51,7 @@ function zeroCalc() {
   }
 
   const sum = n1_cnt * n1_cnt + n2_cnt * n2_cnt;
-
-  // console.log("zero");
-  // console.log(sum.toString());
   return sum;
 }
+
+console.log(solution().toString());
