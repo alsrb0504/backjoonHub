45ad812a653bc051fcd4699a3f39b9014942ec03
MinKyu Tae <alsrb8442@naver.com>
2022-12-26 17:02:24 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1696 ms, Memory: 225064 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.js
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.js
@@ -8,42 +8,27 @@ const N = Number(input[0]);
 const nums = input[1].split(" ").map(Number);
 const tc = Number(input[2]);
 const answer = [];
-
 const dp = Array.from({ length: N }, () => new Array(N).fill(false));
 
 for (let i = 0; i < N; i++) {
-  const stack = [nums[i]];
-  const std = nums[i];
   dp[i][i] = true;
+  if (i + 1 < N) {
+    if (nums[i] === nums[i + 1]) dp[i][i + 1] = true;
+    else dp[i][i + 1] = false;
+  }
+}
 
-  for (let j = i + 1; j < N; j++) {
-    let src = 0;
-    let dst = j - i;
+for (let j = 2; j < N; j++) {
+  for (let i = 0; i < N - 1; i++) {
+    if (nums[i] !== nums[j]) continue;
 
-    stack.push(nums[j]);
-
-    // 시작과 끝이 다르면 X.
-    if (std !== nums[j]) continue;
-
-    let isPossible = true;
-
-    // 투 포인터
-    while (src <= dst && isPossible) {
-      if (stack[src] !== stack[dst]) {
-        isPossible = false;
-        break;
-      }
-
-      src++;
-      dst--;
-    }
-
-    if (isPossible) dp[i][j] = true;
+    if (dp[i + 1][j - 1]) dp[i][j] = true;
   }
 }
 
 input.slice(3, 3 + tc).forEach((el) => {
   const [src, dst] = el.split(" ").map(Number);
+
   if (dp[src - 1][dst - 1]) answer.push(1);
   else answer.push(0);
 });
