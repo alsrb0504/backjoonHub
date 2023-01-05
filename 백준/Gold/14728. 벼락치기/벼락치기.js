9af7dae17857242ae3f0cea45f879307dd80ada5
MinKyu Tae <alsrb8442@naver.com>
2023-01-05 17:14:19 +0900

    [Gold V] Title: 벼락치기, Time: 220 ms, Memory: 24884 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.js
+++ 백준/Gold/14728. 벼락치기/벼락치기.js
@@ -6,26 +6,19 @@ let input = require("fs")
 
 const [N, T] = input[0].split(" ").map(Number);
 const data = input.slice(1, 1 + N).map((el) => el.split(" ").map(Number));
-const dp = Array.from({ length: N + 1 }, () => new Array(T + 1).fill(-1));
-
-dp[0][T] = 0;
+const dp = Array.from({ length: N + 1 }, () => new Array(T + 1).fill(0));
+let answer = 0;
 
 for (let i = 1; i <= N; i++) {
   const [cost, benefit] = data[i - 1];
 
   for (let j = 0; j <= T; j++) {
-    if (j + cost <= T && dp[i - 1][j + cost] !== -1) {
-      dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j + cost] + benefit);
+    if (j < cost) {
+      dp[i][j] = dp[i - 1][j];
+    } else {
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost] + benefit);
     }
-
-    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
   }
 }
 
-let answer = 0;
-
-for (let i = 0; i <= T; i++) {
-  answer = Math.max(answer, dp[N][i]);
-}
-
-console.log(answer);
+console.log(dp[N][T]);
