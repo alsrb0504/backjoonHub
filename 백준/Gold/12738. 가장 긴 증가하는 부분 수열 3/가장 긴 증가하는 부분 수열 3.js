02dba33e95912839437e3ce276f96b7c8250c69e
MinKyu Tae <alsrb8442@naver.com>
2023-01-09 11:38:45 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 732 ms, Memory: 134412 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.js
@@ -0,0 +1,30 @@
+const readFileSyncAddress = "/dev/stdin";
+const input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .split("\n");
+
+const N = Number(input[0]);
+const nums = input[1].split(" ").map(Number);
+const lis = [nums[0]];
+
+for (let i = 1; i < N; i++) {
+  const curr = nums[i];
+  let left = 0;
+  let right = lis.length;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (curr <= lis[mid]) right = mid;
+    else left = mid + 1;
+  }
+
+  if (right === lis.length) {
+    lis.push(curr);
+  } else {
+    lis[right] = curr;
+  }
+}
+
+console.log(lis.length);
