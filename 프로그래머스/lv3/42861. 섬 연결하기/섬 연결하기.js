04a0b3adfc0437af90114acb4a16b23863d71d13
MinKyu Tae <alsrb8442@naver.com>
2023-02-24 14:55:35 +0900

    [level 3] Title: 섬 연결하기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,46 +1,4 @@
 function solution(n, costs) {
-  let answer = 0;
-
-  const arr = new Array(n).fill(0);
-  for (let i = 1; i < n; i++) arr[i] = i;
-
-  costs.sort((a, b) => a[2] - b[2]);
-
-  for (let i = 0; i < costs.length; i++) {
-    const [start, dest, cost] = costs[i];
-
-    if (CheckSameParent(start, dest)) continue;
-
-    Union(start, dest);
-
-    answer += cost;
-  }
-
-  return answer;
-
-  function GetParent(num) {
-    if (num === arr[num]) return num;
-
-    arr[num] = GetParent(arr[num]);
-    return arr[num];
-  }
-
-  function Union(a, b) {
-    const aParent = GetParent(a);
-    const bParent = GetParent(b);
-
-    if (aParent < bParent) {
-      arr[bParent] = aParent;
-    } else {
-      arr[aParent] = bParent;
-    }
-  }
-
-  function CheckSameParent(a, b) {
-    const aParent = GetParent(a);
-    const bParent = GetParent(b);
-
-    if (aParent === bParent) return true;
-    else return false;
-  }
+    var answer = 0;
+    return answer;
 }
