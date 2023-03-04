11ab0586da996616f00d2c807d9c0831230a432e
MinKyu Tae <alsrb8442@naver.com>
2023-03-05 02:14:18 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 16.27 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,46 +1,39 @@
 function solution(maps) {
-    const endY = maps.length - 1;
-    const endX = maps[0].length - 1;
-    
-    
-    // 효율성 테스트 시간 초과 방지.
-    if(endY > 1 && endX > 1) {
-        if(maps[endY - 1][endX] === 0 && maps[endY - 1][endX - 1] === 0 && maps[endY][endX - 1] === 0) return -1;
-    }
-    
-    return bfs();
-    
-    function bfs() {
-        const moves = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-        const q = [{x: 0, y: 0, cnt: 1}];
+  const dy = [0, 0, -1, 1];
+  const dx = [-1, 1, 0, 0];
 
-        while(q.length > 0) {
-            const {x, y, cnt} = q.shift();
-            
-            for(let i = 0; i < 4; i++) {
-                const [moveX, moveY] = moves[i];
-                const nextX = moveX + x;
-                const nextY = moveY + y;
-                
-                // 범위 확인
-                if(nextX < 0 || nextX > endX || nextY < 0 || nextY > endY) continue;
-                // 벽 확인
-                if(maps[nextY][nextX] === 0) continue; 
-                
-                // 종료 조건 확인
-                if(nextX === endX && nextY === endY) {
-                    return cnt + 1;
-                }
+  const H = maps.length;
+  const W = maps[0].length;
 
-                // 아 그냥 값이 1보다 크면 방문한거지
-                // [1, 1]은 예외.
-                if(maps[nextY][nextX] < 2) {
-                    maps[nextY][nextX] = cnt + 1;
-                    q.push({x: nextX, y: nextY, cnt: cnt + 1});
-                }
-            }
-        }
-        // 갈 수 없을 경우.
-        return -1;
+  const visited = Array.from({ length: H }, () => new Array(W).fill(Infinity));
+
+  bfs();
+
+  return visited[H - 1][W - 1] === Infinity ? -1 : visited[H - 1][W - 1];
+
+  function bfs() {
+    const q = [[0, 0, 1]];
+    visited[0][0] = 0;
+
+    while (q.length) {
+      const [y, x, cnt] = q.shift();
+
+      for (let i = 0; i < 4; i++) {
+        const [ny, nx] = [y + dy[i], x + dx[i]];
+
+        if (
+          ny < 0 ||
+          nx < 0 ||
+          ny >= H ||
+          nx >= W ||
+          maps[ny][nx] === 0 ||
+          visited[ny][nx] <= cnt + 1
+        )
+          continue;
+
+        visited[ny][nx] = cnt + 1;
+        q.push([ny, nx, cnt + 1]);
+      }
     }
+  }
 }
