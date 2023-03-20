2ff513aced10e25354e8f1a41f25c6592378e3d1
MinKyu Tae <alsrb8442@naver.com>
2023-03-20 11:45:43 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 342.34 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,122 +1,167 @@
-class maxHeap {
+// 2차원 최대힙
+class MaxHeap {
   constructor() {
-    this.q = [null];
+    this.heap = [null];
   }
 
   size() {
-    return this.q.length;
+    return this.heap.length - 1;
   }
 
-  heapPush(item) {
-    this.q.push(item);
+  add(num) {
+    if (this.size() === 0) {
+      this.heap.push(num);
+      return;
+    }
 
-    let curidx = this.size() - 1;
-    let paridx = Math.floor(curidx / 2);
+    this.heap.push(num);
 
-    // 부모가 자신보다 커야함.
-    while (this.q[curidx] > this.q[paridx] && curidx !== 1) {
-      [this.q[curidx], this.q[paridx]] = [this.q[paridx], this.q[curidx]];
-      curidx = paridx;
-      paridx = Math.floor(curidx / 2);
+    let cur_idx = this.size();
+    let par_idx = Math.floor(cur_idx / 2);
+
+    // [거리, rest]
+    while (cur_idx > 1 && this.heap[cur_idx][0] > this.heap[par_idx][0]) {
+      [this.heap[cur_idx], this.heap[par_idx]] = [
+        this.heap[par_idx],
+        this.heap[cur_idx],
+      ];
+
+      cur_idx = par_idx;
+      par_idx = Math.floor(cur_idx / 2);
     }
   }
 
-  heapPop() {
-    // 빈 경우
-    if (this.size() === 1) return -1;
+  pop() {
+    const curr_size = this.size();
 
-    const min = this.q[1];
+    if (curr_size === 0) {
+      return -1;
+    }
 
-    if (this.size() <= 2) this.q = [null];
-    else this.q[1] = this.q.pop();
+    if (curr_size === 1) {
+      return this.heap.pop();
+    }
 
-    let curidx = 1;
-    let leftidx = curidx * 2;
-    let rightidx = curidx * 2 + 1;
+    const max = this.heap[1];
+    this.heap[1] = this.heap.pop();
 
-    // 자식이 없는 경우
-    if (this.q[leftidx] === null) return min;
+    let cur_idx = 1;
+    let lt_idx = cur_idx * 2;
+    let rt_idx = cur_idx * 2 + 1;
 
-    // 자식이 하나
-    if (this.q[rightidx] === null) {
-      if (this.q[curidx] < this.q[leftidx]) {
-        [this.q[curidx], this.q[leftidx]] = [this.q[leftidx], this.q[curidx]];
+    // 자식이 없다면
+    if (!this.heap[lt_idx]) {
+      return max;
+    }
+
+    // 자식이 하나라면
+    if (!this.heap[rt_idx]) {
+      if (this.heap[cur_idx][0] < this.heap[lt_idx][0]) {
+        this.swap(cur_idx, lt_idx);
       }
-      return min;
+      return max;
     }
 
-    // 자식이 둘 다 존재.
+    // 자식이 둘 이상
     while (
-      this.q[curidx] < this.q[leftidx] ||
-      this.q[curidx] < this.q[rightidx]
+      lt_idx < this.size() &&
+      (this.heap[lt_idx][0] > this.heap[cur_idx][0] ||
+        this.heap[rt_idx][0] > this.heap[cur_idx][0])
     ) {
-      const maxidx = this.q[leftidx] < this.q[rightidx] ? rightidx : leftidx;
+      const max_idx =
+        this.heap[lt_idx][0] < this.heap[rt_idx][0] ? rt_idx : lt_idx;
 
-      [this.q[curidx], this.q[maxidx]] = [this.q[maxidx], this.q[curidx]];
+      this.swap(cur_idx, max_idx);
 
-      curidx = maxidx;
-      leftidx = maxidx * 2;
-      rightidx = maxidx * 2 + 1;
-      //
+      cur_idx = max_idx;
+      lt_idx = cur_idx * 2;
+      rt_idx = cur_idx * 2 + 1;
     }
 
-    return min;
+    return max;
+  }
+
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
   }
 }
 
+// const cap = 4;
+// const n = 5;
+// const deliveries = [1, 0, 3, 1, 2];
+// const pickups = [0, 3, 0, 4, 0];
+
+const cap = 2;
+const n = 7;
+const deliveries = [1, 0, 2, 0, 1, 0, 2];
+const pickups = [0, 2, 0, 1, 0, 2, 0];
+
+console.log(solution(cap, n, deliveries, pickups));
+
 function solution(cap, n, deliveries, pickups) {
+  const del_heap = new MaxHeap();
+  const pickup_heap = new MaxHeap();
   let answer = 0;
 
-  const del_heap = new maxHeap();
-  const pick_heap = new maxHeap();
-
-  let del_cnt = 0;
-  let pick_cnt = 0;
-
-  deliveries.forEach((el, idx) => {
-    if (el !== 0) {
-      for (let i = 0; i < el; i++) {
-        del_heap.heapPush(idx + 1);
-
-        del_cnt++;
-      }
-    }
+  deliveries.forEach((val, idx) => {
+    if (val > 0) del_heap.add([idx + 1, val]);
+  });
+  pickups.forEach((val, idx) => {
+    if (val > 0) pickup_heap.add([idx + 1, val]);
   });
 
-  pickups.forEach((el, idx) => {
-    if (el !== 0) {
-      for (let i = 0; i < el; i++) {
-        pick_heap.heapPush(idx + 1);
+  while (del_heap.size() || pickup_heap.size()) {
+    // console.table(del_heap);
+    // console.table(pickup_heap);
 
-        pick_cnt++;
+    let del_dist = 0;
+    let pickup_dist = 0;
+
+    let del_cnt = 0;
+    let pickup_cnt = 0;
+
+    while (del_cnt < cap && del_heap.size()) {
+      const [dist, rest] = del_heap.pop();
+      const sum = del_cnt + rest;
+
+      // 최대 배달 거리 갱신
+      del_dist = Math.max(del_dist, dist);
+
+      if (sum <= cap) {
+        del_cnt = sum;
+      } else {
+        const diff = sum - cap;
+
+        del_cnt = cap;
+
+        del_heap.add([dist, diff]);
       }
     }
-  });
 
-  // console.table(del_heap.q);
-  // console.table(pick_heap.q);
+    while (pickup_cnt < cap && pickup_heap.size()) {
+      const [dist, rest] = pickup_heap.pop();
+      const sum = pickup_cnt + rest;
 
-  // console.log(`del_cnt = ${del_cnt}, pick_cnt = ${pick_cnt}`);
+      // 최대 배달 거리 갱신
+      pickup_dist = Math.max(pickup_dist, dist);
 
-  while (del_cnt > 0 || pick_cnt > 0) {
-    let dist = 0;
+      if (sum <= cap) {
+        pickup_cnt = sum;
+      } else {
+        const diff = sum - cap;
+        pickup_cnt = cap;
 
-    for (let i = 0; i < cap && del_cnt > 0; i++) {
-      const tmp = del_heap.heapPop();
-      dist = Math.max(dist, tmp);
-
-      del_cnt--;
+        pickup_heap.add([dist, diff]);
+      }
     }
 
-    for (let i = 0; i < cap && pick_cnt > 0; i++) {
-      const tmp = pick_heap.heapPop();
-      dist = Math.max(dist, tmp);
+    // console.log(`del_cnt = ${del_cnt}`);
+    // console.log(`pickup_cnt = ${pickup_cnt}`);
+    // console.log(Math.max(del_dist, pickup_dist));
 
-      pick_cnt--;
-    }
-
-    answer += 2 * dist;
+    answer += Math.max(del_dist, pickup_dist) * 2;
   }
 
   return answer;
 }
+
