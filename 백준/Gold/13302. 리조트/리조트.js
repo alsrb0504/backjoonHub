b6bbba93ec44ea80d9a1f74a982ec61c1f6a5396
MinKyu Tae <alsrb8442@naver.com>
2023-03-22 16:34:49 +0900

    [Gold IV] Title: 리조트, Time: 128 ms, Memory: 9472 KB -BaekjoonHub

---
+++ 백준/Gold/13302. 리조트/리조트.js
@@ -0,0 +1,60 @@
+const readFileSyncAddress = "/dev/stdin";
+const input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trimEnd()
+  .split("\n");
+
+const INF = Infinity;
+const [N, M] = input[0].split(" ").map(Number);
+const block = new Set();
+const MAX_TICKET = 110;
+
+if (M !== 0) {
+  const arr = input[1].split(" ").map(Number);
+  arr.forEach((el) => {
+    block.add(el);
+  });
+}
+
+const dp = Array.from({ length: MAX_TICKET }, () =>
+  new Array(MAX_TICKET).fill(INF)
+);
+
+dp[0][0] = 0;
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < MAX_TICKET; j++) {
+    if (dp[j][i] === INF) continue;
+
+    const curr = dp[j][i];
+
+    // 바로 다음칸
+    if (block.has(i + 1)) {
+      dp[j][i + 1] = Math.min(dp[j][i + 1], curr);
+    } else {
+      dp[j][i + 1] = Math.min(dp[j][i + 1], curr + 10000);
+    }
+    // 3일
+    for (let k = 1; k <= 3; k++) {
+      dp[j + 1][i + k] = Math.min(dp[j + 1][i + k], curr + 25000);
+    }
+    // 5일
+    for (let k = 1; k <= 5; k++) {
+      dp[j + 2][i + k] = Math.min(dp[j + 2][i + k], curr + 37000);
+    }
+
+    // 감소
+    if (j >= 3) {
+      dp[j - 3][i + 1] = Math.min(dp[j - 3][i + 1], curr);
+    }
+  }
+}
+
+let answer = INF;
+
+for (let i = 0; i < MAX_TICKET; i++) {
+  answer = Math.min(answer, dp[i][N]);
+}
+
+console.log(answer);
