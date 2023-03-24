ad9e4890b8db13955c0baa202486a1cf9b0823c5
MinKyu Tae <alsrb8442@naver.com>
2023-03-24 11:55:01 +0900

    [Silver IV] Title: 상근이의 여행, Time: 332 ms, Memory: 37080 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.js
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.js
@@ -2,51 +2,26 @@ const readFileSyncAddress = "/dev/stdin";
 const input = require("fs")
   .readFileSync(readFileSyncAddress)
   .toString()
+  .trimEnd()
   .split("\n");
 
-const TC = Number(input[0]);
+const INF = Infinity;
+const tc = Number(input[0]);
 const answer = [];
-let lc = 1;
+let line = 1;
 
-for (let i = 0; i < TC; i++) {
-  const [result, next] = solution(lc);
-
-  lc += next + 1;
-  answer.push(result);
-}
-
-console.table(answer.join("\n"));
-
-function solution(line) {
+for (let i = 0; i < tc; i++) {
   const [N, M] = input[line].split(" ").map(Number);
-  const board = Array.from({ length: N }, () => new Array(N).fill(false));
-  const visited = new Array(N).fill(false);
 
-  input.slice(line + 1, line + 1 + M).forEach((el) => {
-    const [s, e] = el.split(" ").map((n) => Number(n) - 1);
-    board[s][e] = true;
-    board[e][s] = true;
-  });
+  answer.push(solution(N, M));
+  line += M + 1;
+}
+console.log(answer.join("\n"));
 
-  return [bfs(), M];
+function solution(N, M) {
+  const edges = input
+    .slice(line + 1, line + 1 + M)
+    .map((el) => el.split(" ").map(Number));
 
-  function bfs() {
-    const q = [0];
-    let cnt = 0;
-    visited[0] = true;
-
-    while (q.length) {
-      const curr = q.shift();
-
-      for (let i = 0; i < N; i++) {
-        if (!visited[i] && board[curr][i]) {
-          visited[i] = true;
-          q.push(i);
-          cnt++;
-        }
-      }
-    }
-
-    return cnt;
-  }
+  return N - 1;
 }
