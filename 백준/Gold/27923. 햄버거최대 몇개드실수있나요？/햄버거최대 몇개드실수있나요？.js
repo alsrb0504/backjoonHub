ebd4636d40892bc0da55d1dc265f756f2d7cb007
MinKyu Tae <alsrb8442@naver.com>
2023-04-03 12:01:05 +0900

    [Gold IV] Title: 햄버거최대 몇개드실수있나요?, Time: 572 ms, Memory: 75916 KB -BaekjoonHub

---
+++ 백준/Gold/27923. 햄버거최대 몇개드실수있나요？/햄버거최대 몇개드실수있나요？.js
@@ -0,0 +1,49 @@
+const readFileSyncAddress = "/dev/stdin";
+const input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trimEnd()
+  .split("\n");
+
+const [N, K, L] = input[0].split(" ").map(Number);
+const hamburgeres = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => b - a);
+
+const cokes = input[2].split(" ").map(Number);
+const cokeMap = new Map();
+
+cokes.forEach((coke) => {
+  cokeMap.set(coke, cokeMap.has(coke) ? cokeMap.get(coke) + 1 : 1);
+});
+
+const cokeEffects = new Array(N + 1).fill(0);
+const cokeAcc = new Array(N + 1).fill(0);
+let hamburger_idx = 0;
+let answer = 0;
+
+for (let [key, val] of cokeMap) {
+  cokeEffects[key] += val;
+  if (key + L <= N) cokeEffects[key + L] -= val;
+}
+
+for (let i = 1; i <= N; i++) {
+  cokeAcc[i] = cokeAcc[i - 1] + cokeEffects[i];
+}
+
+cokeAcc.sort((a, b) => b - a);
+
+hamburgeres.forEach((hambur, idx) => {
+  if (cokeAcc[idx] === 0) answer += hambur;
+  else {
+    while (hambur !== 0 && cokeAcc[idx] > 0) {
+      hambur = Math.floor(hambur / 2);
+      cokeAcc[idx]--;
+    }
+
+    answer += hambur;
+  }
+});
+
+console.log(answer);
