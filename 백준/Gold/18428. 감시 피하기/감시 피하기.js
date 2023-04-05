6e6c07adc85417ea7405cbd35fd6524a9ac0de5d
MinKyu Tae <alsrb8442@naver.com>
2023-04-05 14:20:40 +0900

    [Gold V] Title: 감시 피하기, Time: 192 ms, Memory: 11724 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.js
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.js
@@ -11,6 +11,7 @@ const map = input.slice(1, 1 + N).map((el) => el.trimEnd().split(" "));
 const teachers = [];
 const spaces = [];
 
+// 선생님과 빈 공간의 위치를 저장
 for (let y = 0; y < N; y++) {
   for (let x = 0; x < N; x++) {
     if (map[y][x] === "T") teachers.push([y, x]);
@@ -18,8 +19,12 @@ for (let y = 0; y < N; y++) {
   }
 }
 
+// dfs 조합과 부르트포스 탐색을 통해 결과 도출
+// 최대 계산 : 벽의 조합(34 C 3 => 5984) * 선생님의 탐색(최대 32 * 12)
+// 따라서 대략 10^5 ~ 10^6의 연산이므로 브루트포스 가능
 console.log(dfs(0, 0) ? "YES" : "NO");
 
+/** 선생님들이 학생을 발견하는지 확인하는 함수 */
 function isPossible() {
   for (let i = 0; i < teachers.length; i++) {
     const [startY, startX] = teachers[i];
@@ -49,6 +54,8 @@ function isPossible() {
   return true;
 }
 
+/** 빈 공간의 조합을 만들어주는 dfs */
+// 3개의 벽을 세우고, 조건을 통과하는지 확인
 function dfs(cnt, start) {
   if (cnt === 3) {
     if (isPossible()) return true;
