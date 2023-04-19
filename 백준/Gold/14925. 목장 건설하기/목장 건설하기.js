ef9ae30ab3ac5fa6afebaad81303f04c99d850d1
MinKyu Tae <alsrb8442@naver.com>
2023-04-19 14:43:37 +0900

    [Gold IV] Title: 목장 건설하기, Time: 400 ms, Memory: 42628 KB -BaekjoonHub

--- 백준/Gold/14925. 목장 건설하기/목장 건설하기.js
+++ 백준/Gold/14925. 목장 건설하기/목장 건설하기.js
@@ -9,7 +9,6 @@ const [H, W] = input[0].split(" ").map(Number);
 const map = input.slice(1, 1 + H).map((el) => el.split(" ").map(Number));
 const dp = Array.from({ length: H + 1 }, () => new Array(W + 1).fill(1));
 
-// 오른쪽, 오른쪽 아래 대각선, 아래쪽
 const dy = [0, 1, 1];
 const dx = [1, 1, 0];
 let answer = 0;
@@ -26,15 +25,8 @@ for (let x = 0; x <= W; x++) dp[H][x] = 0;
 for (let y = H - 1; y >= 0; y--) {
   for (let x = W - 1; x >= 0; x--) {
     if (map[y][x] === 0) {
-      const squreSet = new Set();
+      dp[y][x] = Math.min(dp[y][x + 1], dp[y + 1][x + 1], dp[y + 1][x]) + 1;
 
-      for (let i = 0; i < 3; i++) {
-        const [ny, nx] = [y + dy[i], x + dx[i]];
-
-        squreSet.add(dp[ny][nx]);
-      }
-
-      dp[y][x] = Math.min(...squreSet) + 1;
       answer = Math.max(answer, dp[y][x]);
     }
   }
