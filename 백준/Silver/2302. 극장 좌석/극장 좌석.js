be77ebac8cff49ae845d66e423056d08a3c05867
MinKyu Tae <alsrb8442@naver.com>
2023-04-23 16:41:39 +0900

    [Silver I] Title: 극장 좌석, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.js
@@ -0,0 +1,37 @@
+const readFileSyncAddress = "/dev/stdin";
+const input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trimEnd()
+  .split("\n");
+
+const N = Number(input[0]);
+const M = Number(input[1]);
+const fixed = [];
+let answer = 1;
+
+const dp = new Array(41).fill(1);
+dp[2] = 2;
+
+for (let i = 3; i <= 40; i++) dp[i] = dp[i - 2] + dp[i - 1];
+
+if (M === 0) {
+  console.log(dp[N]);
+  return;
+}
+
+input.slice(2, 2 + M).forEach((el) => {
+  fixed.push(Number(el));
+});
+
+fixed.push(N + 1);
+
+let prev = 1;
+fixed.forEach((fix) => {
+  const diff = fix - prev;
+
+  answer *= dp[diff];
+  prev = fix + 1;
+});
+
+console.log(answer);
