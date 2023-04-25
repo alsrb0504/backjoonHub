a21898b14faa7aa34278cc128c1fdec75f7eea4d
MinKyu Tae <alsrb8442@naver.com>
2023-04-25 12:25:16 +0900

    [level 2] Title: 숫자 블록, Time: 92.34 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12923. 숫자 블록/숫자 블록.js
+++ 프로그래머스/lv2/12923. 숫자 블록/숫자 블록.js
@@ -1,4 +1,28 @@
 function solution(begin, end) {
+    let answer = [];
+
+    for(let i=begin; i<=end; i++){
+        answer.push(findN(i));
+    }
+    return answer;
+} 
+
+function findN(number){
+    if(number===1) return 0;
+
+    let temp = 1;
+    for(let i=2 ; i<=Math.sqrt(number); i++){
+        if(number%i===0){
+            temp = i;
+            // 10**7인데 10*7로 써서 한시간동안 에러 찾음...;;
+            // 예시케이스는 되는데 테케는 왜 안되나...내가 멀 잘못햇나..세상이 날 억까한다...
+            if(number/i<=10**7) return number/i;
+        }
+    }
+    return temp;
+}
+/*
+function solution(begin, end) {
   const SIZE = end - begin + 1;
   const MAX_NUM = 1e7;
   const answer = new Array(SIZE).fill(1);
@@ -30,3 +54,4 @@ function solution(begin, end) {
     return max;
   }
 }
+*/
