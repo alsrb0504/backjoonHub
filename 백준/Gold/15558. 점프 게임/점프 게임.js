b21fe5f050f501873b2b778e9c309e9ad1570ff3
MinKyu Tae <alsrb8442@naver.com>
2023-05-12 18:52:53 +0900

    [Gold V] Title: 점프 게임, Time: 524 ms, Memory: 59064 KB -BaekjoonHub

---
+++ 백준/Gold/15558. 점프 게임/점프 게임.js
@@ -0,0 +1,138 @@
+const readFileSyncAddress = "/dev/stdin";
+const input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trimEnd()
+  .split("\n");
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.rear = null;
+    this.length = 0;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  enqueue(data) {
+    const node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      this.rear.next = node;
+    }
+
+    this.rear = node;
+    this.length++;
+  }
+
+  dequeue() {
+    if (!this.head) {
+      return false;
+    }
+    const data = this.head.data;
+    this.head = this.head.next;
+    this.length--;
+
+    return data;
+  }
+}
+
+const [N, K] = input[0].split(" ").map(Number);
+
+const path = [];
+
+// const visited = Array.from({ length: 2 }, () => new Array(N + 1).fill(0));
+const visited = new Set();
+
+path.push(input[1].trimEnd().split("").map(Number));
+path.push(input[2].trimEnd().split("").map(Number));
+path[0].unshift(-1);
+path[1].unshift(-1);
+
+const q = new Queue();
+q.enqueue({ currX: 1, currY: 0, currT: 0 });
+// const q = [{ currX: 1, currY: 0, currT: 0 }];
+
+while (q.size()) {
+  // const { currX, currY, currT } = q.shift();
+
+  const { currX, currY, currT } = q.dequeue();
+
+  // console.log(currX, currY, currT);
+
+  const nextX = currX + 1;
+  const backX = currX - 1;
+  const crossX = currX + K;
+  // const blockX = currT;
+  const blockX = currT + 1; // 앞으로 없어질 칸
+
+  // 앞으로 한 칸 이동 가능
+  if (path[currY][nextX] === 1 && blockX !== nextX && nextX !== currT) {
+    // 종료
+    if (nextX > N) {
+      console.log(1);
+      return;
+    }
+
+    const key = makeKey(currY, nextX, currT + 1);
+
+    if (!visited.has(key)) {
+      visited.add(key);
+      // q.push({ currX: nextX, currY, currT: currT + 1 });
+      q.enqueue({ currX: nextX, currY, currT: currT + 1 });
+    }
+  }
+
+  // 뒤로 한 칸 이동 가능
+  if (path[currY][backX] === 1 && blockX !== backX && backX !== currT) {
+    // q.push({ currX: backX, currY, currT: currT + 1 });
+
+    const key = makeKey(currY, backX, currT + 1);
+
+    if (!visited.has(key)) {
+      visited.add(key);
+      q.enqueue({ currX: backX, currY, currT: currT + 1 });
+    }
+  }
+
+  // 대각선 건너 이동 가능
+  // 앞으로 한 칸 이동 가능
+  if (blockX !== crossX) {
+    const nextCrossY = currY === 1 ? 0 : 1;
+
+    // 종료
+    if (crossX > N) {
+      console.log(1);
+
+      return;
+    }
+
+    if (crossX <= N && path[nextCrossY][crossX] === 1) {
+      if (crossX !== blockX && crossX !== currT) {
+        const key = makeKey(nextCrossY, crossX, currT + 1);
+
+        if (!visited.has(key)) {
+          visited.add(key);
+          // q.push({ currX: crossX, currY: nextCrossY, currT: currT + 1 });
+          q.enqueue({ currX: crossX, currY: nextCrossY, currT: currT + 1 });
+        }
+      }
+    }
+  }
+}
+
+function makeKey(y, x, t) {
+  return `${y}-${x}-${t}`;
+}
+
+console.log(0);
