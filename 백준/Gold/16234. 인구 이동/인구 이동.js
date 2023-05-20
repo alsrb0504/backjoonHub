af7bf73aa37c6d56565891dee806212a798f1e7f
MinKyu Tae <alsrb8442@naver.com>
2023-05-20 21:37:48 +0900

    [Gold V] Title: 인구 이동, Time: 1176 ms, Memory: 28948 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.js
+++ 백준/Gold/16234. 인구 이동/인구 이동.js
@@ -1,4 +1,6 @@
-const readFileSyncAddress = "/dev/stdin";
+const readFileSyncAddress =
+  process.platform === "linux" ? "/dev/stdin" : "text.txt";
+
 const input = require("fs")
   .readFileSync(readFileSyncAddress)
   .toString()
@@ -12,13 +14,18 @@ const [N, L, R] = input[0].split(" ").map(Number);
 const board = input.slice(1, 1 + N).map((el) => el.split(" ").map(Number));
 
 let visited = Array.from({ length: N }, () => new Array(N).fill(false));
-let isPossible = false;
-
 let answer = 0;
 
-while (!isPossible) {
+// while문이 한 번 실행될 때마다
+// 국경을 공유하는 것이 가능한 경우가 있다면 annswer++ 후, 반복
+// 국경을 공유할 수 없다면 종료
+while (true) {
+  let isPossible = false; // 국경 공유가 가능한지 판단하는 변수
+
+  // 매번 while 반복마다 방문 배열을 초기화
   visited = Array.from({ length: N }, () => new Array(N).fill(false));
 
+  // 국경 공유가 가능하다면 방문처리, 값 분배, isPossible에 참 할당
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
       if (!visited[i][j]) {
@@ -29,15 +36,17 @@ while (!isPossible) {
     }
   }
 
-  if (isPossible) {
-    answer++;
-    isPossible = false;
-  } else break;
+  if (isPossible) answer++;
+  else break;
 }
 
+// 정답 출력
 console.log(answer);
 
 function bfs(y, x) {
+  // bfs로 탐색하면서 값을 공유할 나라의 인덱스는 stack에
+  // 나라의 값들은 acc(누적합)에 저장
+  // 이후, stack과 acc를 활용하여 국경이 공유된 나라에 동일한 값을 할당
   let stack = [[y, x]];
   let q = [[y, x]];
   let acc = board[y][x];
