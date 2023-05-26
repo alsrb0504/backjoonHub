eb84097ea8b04aa99ecdc367908835e0377a7134
MinKyu Tae <alsrb8442@naver.com>
2023-05-26 18:22:02 +0900

    [Silver II] Title: 색종이 만들기, Time: 184 ms, Memory: 10756 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.js
@@ -0,0 +1,66 @@
+const readFileSyncAddress =
+  process.platform === "linux" ? "/dev/stdin" : "text.txt";
+
+const input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trimEnd()
+  .split("\n");
+
+const N = Number(input[0]);
+const map = input
+  .slice(1, 1 + N)
+  .map((el) => el.trimEnd().split(" ").map(Number));
+
+map.forEach((el) => {
+  el.unshift(-1);
+});
+
+map.unshift(-1);
+
+let whiteCnt = 0;
+let blueCnt = 0;
+
+solution(1, 1, N, N, N);
+
+console.log(whiteCnt + "\n" + blueCnt);
+
+function solution(y1, x1, y2, x2, size) {
+  let isSame = true;
+  let fir = map[y1][x1];
+
+  for (let i = y1; i <= y2; i++) {
+    for (let j = x1; j <= x2; j++) {
+      if (fir !== map[i][j]) {
+        isSame = false;
+        break;
+      }
+    }
+  }
+
+  if (isSame) {
+    countColor(fir);
+    return;
+  }
+
+  const half = size / 2;
+  const half_x = x1 + half - 1;
+  const half_y = y1 + half - 1;
+
+  if (size > 2) {
+    solution(y1, x1, half_y, half_x, half);
+    solution(y1, half_x + 1, half_y, x2, half);
+    solution(half_y + 1, x1, y2, half_x, half);
+    solution(half_y + 1, half_x + 1, y2, x2, half);
+  } else {
+    countColor(map[y1][x1]);
+    countColor(map[y1][x2]);
+    countColor(map[y2][x1]);
+    countColor(map[y2][x2]);
+  }
+}
+
+function countColor(color) {
+  if (color === 1) blueCnt++;
+  else whiteCnt++;
+}
