e358cef3c977f8d0aaed9eeca810a074df03ce32
MinKyu Tae <alsrb8442@naver.com>
2023-06-17 12:13:43 +0900

    [Platinum V] Title: 한동이는 영업사원!, Time: 392 ms, Memory: 46484 KB -BaekjoonHub

---
+++ 백준/Platinum/8012. 한동이는 영업사원！/한동이는 영업사원！.js
@@ -0,0 +1,95 @@
+const readFileSyncAddress =
+  process.platform === "linux" ? "/dev/stdin" : "text.txt";
+
+const input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trimEnd()
+  .split("\n");
+
+const N = Number(input[0]);
+const MAX = 30001;
+let answer = 0;
+const LOG = 17;
+
+const graph = Array.from({ length: MAX }, () => []);
+const parent = Array.from({ length: MAX }, () => new Array(LOG).fill(0));
+const visited = new Array(MAX).fill(false);
+const depthArr = new Array(MAX).fill(-1);
+
+// 그래프 연결
+input.slice(1, N).forEach((info) => {
+  const [u, v] = info.split(" ").map(Number);
+
+  graph[u].push(v);
+  graph[v].push(u);
+});
+
+setParent();
+
+const query = Number(input[N]);
+
+let prev = 1;
+input.slice(N + 1, N + 1 + query).forEach((num) => {
+  const nextNode = Number(num);
+  answer += LCA(prev, nextNode);
+  prev = nextNode;
+});
+
+console.log(answer);
+
+function dfs(node, depth) {
+  visited[node] = true;
+  depthArr[node] = depth;
+
+  for (let next of graph[node]) {
+    if (visited[next]) continue;
+
+    parent[next][0] = node;
+    dfs(next, depth + 1);
+  }
+}
+
+// 전체 부모 관계를 설정하는 함수
+function setParent() {
+  dfs(1, 0);
+
+  for (let j = 1; j < LOG; j++) {
+    for (let i = 1; i <= N; i++) {
+      parent[i][j] = parent[parent[i][j - 1]][j - 1];
+    }
+  }
+}
+
+function LCA(x, y) {
+  let cost = 0;
+
+  const [a, b] = [x, y];
+
+  // y의 깊이가 더 깊도록 설정
+  if (depthArr[x] > depthArr[y]) [x, y] = [y, x];
+
+  // 두 노드의 깊이를 동일하게 설정
+  for (let i = LOG - 1; i >= 0; i--) {
+    if (depthArr[y] - depthArr[x] >= 1 << i) {
+      cost += depthArr[y] - depthArr[x];
+
+      y = parent[y][i];
+    }
+  }
+
+  // 부모가 같은 경우 반환
+  if (x === y) return depthArr[a] + depthArr[b] - 2 * depthArr[x];
+
+  for (let i = LOG - 1; i >= 0; i--) {
+    // 조상을 향해 거슬로 올라감
+    if (parent[x][i] !== parent[y][i]) {
+      cost += depthArr[x] + depthArr[y];
+
+      x = parent[x][i];
+      y = parent[y][i];
+    }
+  }
+
+  return depthArr[a] + depthArr[b] - depthArr[parent[x][0]] * 2;
+}
