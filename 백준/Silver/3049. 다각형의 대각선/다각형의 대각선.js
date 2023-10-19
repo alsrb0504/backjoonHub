f1196dc8c4fc00642b646e7f76223bb8c0fb6fa4
MinKyu Tae <alsrb8442@naver.com>
2023-10-19 23:12:40 +0900

    [Silver V] Title: 다각형의 대각선, Time: 116 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/3049. 다각형의 대각선/다각형의 대각선.js
@@ -0,0 +1,12 @@
+const readFileSyncAddress =
+  process.platform === "linux" ? "/dev/stdin" : "text.txt";
+
+const input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trimEnd()
+  .split("\n");
+
+const N = Number(input[0]);
+
+console.log((N * (N - 1) * (N - 2) * (N - 3)) / 24);
